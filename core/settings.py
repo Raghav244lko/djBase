9bab612b5b2e5fb452958fb87fc2b0bd9cab7c39
Raghav244lko <raghav.lko24@gmail.com>
2024-04-19 19:19:37 +0530

    third

--- core/settings.py
+++ core/settings.py
@@ -24,6 +24,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'whitenoise.runserver_nostatic',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
